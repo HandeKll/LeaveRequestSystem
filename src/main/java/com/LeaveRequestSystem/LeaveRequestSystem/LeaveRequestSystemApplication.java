a20a1f62ae0b27bc20a7d306cd6359785b5071ed
HandeKll <handekolel@gmail.com>
2023-09-18 17:15:20 +0300

    Initial commit

---
+++ src/main/java/com/LeaveRequestSystem/LeaveRequestSystem/LeaveRequestSystemApplication.java
@@ -0,0 +1,13 @@
+package com.LeaveRequestSystem.LeaveRequestSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveRequestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveRequestSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LeaveRequestSystem/LeaveRequestSystem/LeaveRequestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LeaveRequestSystem.LeaveRequestSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaveRequestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
